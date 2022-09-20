df890310cdf34323ec87b23a06b6ac5fd7b4a266
Esther Kiiru <kiiruesther66@gmail.com>
2022-09-20 11:19:13 +0000

    Double digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for num in range(0, 100):
-    print("{:d}{:d}, ".format(num))
-    num += num + 1
-    print("{:d}{:d}".format(num))
+for num in range(0, 99):
+    print("{:02d}, ".format(num), end='')
+print("99")
