178f3aa8aee6614975a44e053b4a99c7fe8c826b
Esther Kiiru <kiiruesther66@gmail.com>
2022-09-22 03:24:35 -0700

    calculator_1.py

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,36 @@
+#!/usr/binr/python3
+def add(a, b):
+
+    Args:
+        a: First integer
+        b: Second integer
+
+    Returns:
+        The return value. a + b
+
+def sub(a, b):
+
+    Args:
+        a: First integer
+        b: Second integer
+
+        Returns:
+            The return value. a - b
+
+def mul(a,, b):
+
+    Args:
+        a: First integer
+        b: Second integer
+
+        Returns:
+            The return value. a * b
+
+def div(a, b):
+
+    Args:
+        a: First integer
+        b: Second integer
+
+        Returns:
+            The return value. a / b
