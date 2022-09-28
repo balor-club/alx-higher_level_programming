5a60a4302d9721603e8af30768d75516df266ccd
Esther Kiiru <kiiruesther66@gmail.com>
2022-09-28 10:49:10 -0700

    Biggest integer value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if my_list and len(my_list):
-        num = 0
-        denom = 0
-        for tup in my_list:
-            num += (tup[0] * tup[1])
-            denom += (tup[1])
-        return (num/denom)
-    return 0
+    if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
